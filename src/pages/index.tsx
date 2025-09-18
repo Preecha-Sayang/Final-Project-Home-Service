1a2c487ef08110f60745017261254f35b718fbfb
Preecha <preecha.sayang@gmail.com>
2025-09-18 10:47:01 +0700

    add/server-sql-neondb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import ButtonGhost from "@/components/button/buttonghost";
+import ButtonLarge from "@/components/button/buttonlarge";
+import ButtonPrimary from "@/components/button/buttonprimary";
+import ButtonSecondaryLarge from "@/components/button/buttonseclarge";
+import ButtonSecondary from "@/components/button/buttonsecondary";
 
 
 export default function Home() {
   return (
-   < ButtonGhost disabled={true}  onClick={async () => {
-  await new Promise(res => setTimeout(res, 2000)); // โหลด 2 วินาที
-}}>
-  Submit
-</ ButtonGhost>
-  )
+    <div className="flex gap-3 flrx-col">
+
+ </div> )
 }
