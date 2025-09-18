d174a02e40c83abb6859b370543fd95bbbb7c696
Naranut05 <naranut.sresroy@gmail.com>
2025-09-18 15:37:49 +0700

    add/Navbar-Dropdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,14 @@ import ButtonSecondaryLarge from "@/components/button/buttonseclarge";
 import ButtonSecondary from "@/components/button/buttonsecondary";
 import BookingForm from "@/components/input/use_index";
 import Navbar from "@/components/navbar/Navbar";
-
+import DropdownAdmin from "@/components/dropdown/DropdownAdmin";
+import DropdownUser from "@/components/dropdown/DropdownUser";
 
 export default function Home() {
   return (
     <div className="p-6 flex flex-col gap-10 bg-gray-100 min-h-screen">
       <DropdownUser />
       <DropdownAdmin />
-
       <Navbar />
 
       
