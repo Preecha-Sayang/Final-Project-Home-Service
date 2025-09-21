f99ade191ac62921e5e28d17e45e6f92f360a5b1
Naranut05 <naranut.sresroy@gmail.com>
2025-09-21 16:43:37 +0700

    Edit/payment summary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Prompt } from "next/font/google";
+
+const fontPrompt = Prompt({
+  //Set Font เพื่อใช้ทั้งระบบ ไม่ต้องลบส่วนนี้//
+  variable: "--font-prompt",
+  subsets: ["latin", "thai"],
+  weight: "600",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={fontPrompt.className}>
+      <Component {...pageProps} />;
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,25 @@
-import { useState } from "react";
 import OrderSummary from "@/components/ordersummary/order_summary";
 import Breadcrumb from "@/components/breadcrump/bread_crump";
+import ButtonGhost from "@/components/button/buttonghost";
+import ButtonLarge from "@/components/button/buttonlarge";
+import ButtonPrimary from "@/components/button/buttonprimary";
+import ButtonSecondaryLarge from "@/components/button/buttonseclarge";
+import ButtonSecondary from "@/components/button/buttonsecondary";
+import BookingForm from "@/components/input/use_index";
+import { Footer } from "@/components/footer";
+
+
 
 
 export default function Home() {
-  const [current, setCurrent] = useState("ล้างแอร์");
-
+  
   return (
     <div>
-        {/* Order Summary Section */}
-        <div className="flex min-h-screen items-center justify-center bg-gray-100 p-6">
-          <div className="space-y-6 w-full max-w-md">
-            {/* กล่องแบบเปิดรายละเอียด */}
-            <OrderSummary
-              items={[
-                { name: "9,000 - 18,000 BTU, แบบติดผนัง", quantity: 2 },
-                { name: "9,000 - 18,000 BTU, แบบติดผนัง", quantity: 2 },
-              ]}
-              date="23 เม.ย. 2022"
-              time="11.00 น."
-              address="444/4 คอนโดสุภาลัย เสนานิคม จตุจักร กรุงเทพฯ"
-              promotion={50}
-              total={1550}
-              defaultOpen={true}
-            />
-
-            {/* กล่องแบบปิดรายละเอียดเริ่มต้น */}
-            <OrderSummary
-              items={[
-                { name: "9,000 - 18,000 BTU, แบบติดผนัง", quantity: 2 },
-                { name: "9,000 - 18,000 BTU, แบบติดผนัง", quantity: 2 },
-              ]}
-              date="23 เม.ย. 2022"
-              time="11.00 น."
-              address="444/4 คอนโดสุภาลัย เสนานิคม จตุจักร กรุงเทพฯ"
-              total={1600}
-              defaultOpen={false}
-            />
-          </div>
-        </div>
-        </div>
+     
+      <div>
+        
+      </div>
+      
+    </div>
   );
 }
