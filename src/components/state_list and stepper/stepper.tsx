1c18bd21b59d8e82a2b008ef2188740277533425
Pattaradol <Pattaradol.k@gmail.com>
2025-09-22 15:33:34 +0700

    feat: implement step structure in Stepper component

--- src/components/state_list and stepper/stepper.tsx
+++ src/components/state_list and stepper/stepper.tsx
@@ -1,5 +1,23 @@
 import Image from "next/image";
+import { useState } from "react";
+
+interface Step {
+    id: number;
+    title: string;
+    icon: string;
+  };
 
 export default function Stepper() {
-    
+
+    const steps: Step[] = [
+        { id: 1, title: "รายการ", icon:"/images/icon_document.svg" },
+        { id: 2, title: "กรอกข้อมูลบริการ", icon:"/images/icon_document.svg" },
+        { id: 3, title: "ชำระเงิน", icon:"/images/icon_document.svg" }
+    ]
+
+    return (
+        <div>
+
+        </div>
+    )
 };
