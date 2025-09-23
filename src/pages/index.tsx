60d7babb5073c777a3ed1c27e7b0df9a12c56645
Pattaradol <Pattaradol.k@gmail.com>
2025-09-23 11:21:07 +0700

    feat: update stateList and stepper components with new icons and improved styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import ButtonSecondary from "@/components/button/buttonsecondary";
 
 import { Footer } from "@/components/footer";
 import StateList from "@/components/state_list and stepper/state_list";
+import Stepper from "@/components/state_list and stepper/stepper";
 
 import { Prompt } from "next/font/google";
 
@@ -24,6 +25,7 @@ export default function Home() {
   return (
     //Set Font เพื่อใช้ทั้งระบบ ไม่ต้องลบส่วนนี้//
     <div className={fonrPrompt.className}>
+       <Stepper />
        <StateList />
     </div>
     
