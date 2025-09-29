add5501577db3bea2921d72794dd6ba7a5b8899e
Pattaradol <Pattaradol.k@gmail.com>
2025-09-29 11:53:56 +0700

    feat: Implement category management pages with create and edit functionality

---
+++ src/pages/admin/categories/new.tsx
@@ -0,0 +1,35 @@
+import dynamic from "next/dynamic";
+import BackHeader from "@/components/admin/common/BackHeader";
+
+const ServiceEditor = dynamic(() => import("@/components/admin/categories/editor"), { ssr: false });
+
+export default function NewCategoryPage() {
+    const actions = (
+        <div className="flex gap-2">
+            <a href="/admin/services" className="h-9 inline-flex items-center rounded-lg border border-[var(--gray-200)] bg-white px-3 text-sm text-[var(--gray-800)] hover:bg-[var(--gray-100)] cursor-pointer">
+                ยกเลิก
+            </a>
+            <button
+                type="submit"
+                form="service-form"
+                className="h-9 rounded-lg bg-[var(--blue-600)] px-3 text-sm font-medium text-white hover:bg-[var(--blue-700)] cursor-pointer"
+            >
+                สร้าง
+            </button> {/*ทำแจ้งเตือน popup*/}
+        </div>
+    );
+
+    return (
+        <>
+            <BackHeader
+                subtitle=""
+                title="เพิ่มหมวดหมู่"
+                backHref="/admin/categories"
+                actions={actions}
+            />
+            <ServiceEditor
+                mode="create"
+            />
+        </>
+    );
+}
