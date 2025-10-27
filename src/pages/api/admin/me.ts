9f5ce7080a757a8af46e430d831253b4eb5299c6
Preecha <preecha.sayang@gmail.com>
2025-10-28 06:19:02 +0700

    fix/bug

--- src/pages/api/admin/me.ts
+++ src/pages/api/admin/me.ts
@@ -3,12 +3,12 @@ import { verifyAdminToken } from "lib/server/jwtAdmin";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json({ admin: null });
+  if (!token) return res.status(200).json({ admin: null });
 
   try {
     const admin = verifyAdminToken(token);
     res.status(200).json({ admin });
   } catch (err) {
-    res.status(401).json({ admin: null });
+    res.status(200).json({ admin: null });
   }
 }
